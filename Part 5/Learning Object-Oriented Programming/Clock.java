0e0bc9096c9ac1e73b7c0e0d8089fd76fd118be8
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-03 23:40:05 +0300

    Update Clock.java

--- Part 5/Learning Object-Oriented Programming/Clock.java
+++ Part 5/Learning Object-Oriented Programming/Clock.java
@@ -1,1 +1,12 @@
+int hours = 0;
+int minutes =0;
+int seconds = 0;
+// initiliaze three clock hands and give them initial value to zero
 
+while(true){
+//infinite and conditional loop
+//if( hours<10)
+
+
+
+}
