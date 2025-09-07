7907e6a84932722acbf7051b68d23b98b517d500
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-07 10:25:23 +0300

    Update AmusementParkRide.java

--- Part 6/1. Objects on a list and a list as part of an object/Calculating a Sum from Objects on a List/AmusementParkRide.java
+++ Part 6/1. Objects on a list and a list as part of an object/Calculating a Sum from Objects on a List/AmusementParkRide.java
@@ -1,1 +1,23 @@
+public class AmusementParkRide {
+    private String name;
+    private int minimumHeight;
+    private int visitors;
+    private ArrayList<Person> riding;
 
+    // ..
+
+    public double averageHeightOfPeopleOnRide() {
+        if (riding.isEmpty()) {
+            return -1;
+        }
+
+        int sumOfHeights = 0;
+        for (Person per: riding) {
+            sumOfHeights += per.getHeight();
+        }
+
+        return 1.0 * sumOfHeights / riding.size();
+    }
+
+    // ..
+}
