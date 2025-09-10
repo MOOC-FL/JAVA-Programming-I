829be58e8f1c79155633b9a3d267a5818ae87325
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-10 12:31:17 +0300

    Update UserInterface.java

--- Part 6/2. Separating the user interface from program logic/Create Programe/UserInterface.java
+++ Part 6/2. Separating the user interface from program logic/Create Programe/UserInterface.java
@@ -1,11 +1,36 @@
 public class UserInterface {
     private Scanner scanner;
+        private ArrayList<String> words;
+
    // add scanner 
     public UserInterface(Scanner scanner) {
         this.scanner = scanner;
+        this.words = new ArrayList<String>();
     }
 
     public void start() {
         // do something
+         while (true) {
+            System.out.print("Enter a word: ");
+            String word = scanner.nextLine();
+
+            if (*stop condition*alreadyEntered(word)) {
+                break;
+            }
+                 // adding the word to the list of previous words
+    this.words.add(word);
+
+
+        }
+
+        System.out.println("You gave the same word twice!");
     }
+      public boolean alreadyEntered(String word) {
+        // do something here
+ if (word.equals("end")) {
+        return true;
+    }
+        return false;
+    }
+    
 }
