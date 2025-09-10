d8869a790ab697dcab007dfbb077ade5ea089c47
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-10 12:28:48 +0300

    Update and rename Main.Java to Main.java

---
+++ Part 6/2. Separating the user interface from program logic/Create Programe/Main.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    UserInterface userInterface = new UserInterface(scanner);
+    userInterface.start();
+}
+
