29f00f77431c79411c839a3620a30c62c6b2d959
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-10 12:31:48 +0300

    Update Main.java

--- Part 6/2. Separating the user interface from program logic/Create Programe/Main.java
+++ Part 6/2. Separating the user interface from program logic/Create Programe/Main.java
@@ -2,5 +2,8 @@ public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     UserInterface userInterface = new UserInterface(scanner);
     userInterface.start();
+    for (String word: this.words) {
+    System.out.println(word);
+}
 }
 
