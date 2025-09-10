0f0edc659e4e0f951995ab86a8f446e9243ae63b
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-09-10 12:27:54 +0300

    Create UserInterface.java

---
+++ Part 6/2. Separating the user interface from program logic/Create Programe/UserInterface.java
@@ -0,0 +1,11 @@
+public class UserInterface {
+    private Scanner scanner;
+   // add scanner 
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        // do something
+    }
+}
