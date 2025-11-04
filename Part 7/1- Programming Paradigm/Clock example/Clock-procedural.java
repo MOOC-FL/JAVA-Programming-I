50cd83a0856b0b112657bdc9ad02f50dc6bb2b6d
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-11-04 14:01:33 +0200

    Add procedural clock implementation
    Implement a procedural clock that increments time every second.

--- Part 7/1- Programming Paradigm/Clock example/Clock-procedural.java
+++ Part 7/1- Programming Paradigm/Clock example/Clock-procedural.java
@@ -1,1 +1,40 @@
 
+public class Main {
+    public static void main(String [] args){
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        while (true){
+            print(hours,minutes,seconds)
+            System.out.println();
+
+            seconds = seconds + 1;
+            if (seconds>59){
+                minutes = minutes+1;
+                seconds = 0 ;
+                if(minutes >59){
+                    hours = hours + 1 ;
+                    minutes = 0;
+                    if (hours>23){
+                        hours = 0 ;
+                    }
+                }
+            }
+        }
+
+    public static void print(int hours, int minutes, int seconds) {
+        print(hours);
+        print(minutes);
+        print(seconds);
+    }
+
+    public static void print(int value) {
+        if (value < 10) {
+            System.out.print("0");
+
+            System.out.print(value);
+        }
+
+    }
+
+}
