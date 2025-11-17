3c3444e0580f70dba256f7d47fcf76174e8467d2
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-11-17 11:34:42 +0200

    Update 6-1.java

--- Part 1/6. Conditional statements and conditional operation/Programming Exercise/6-1.java
+++ Part 1/6. Conditional statements and conditional operation/Programming Exercise/6-1.java
@@ -1,1 +1,15 @@
+// Write a program that asks the user for an integer and prints the string "Speeding ticket!" if the input is greater than 120.
+import java.util.Scanner;
 
+public class new {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give speed:");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if(speed>120){
+            System.out.println("Speeding ticket!");
+        }
+    }
+}
