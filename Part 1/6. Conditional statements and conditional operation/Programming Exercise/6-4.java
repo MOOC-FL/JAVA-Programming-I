b63c945f1c2cc1bd793cc4201da14cb665956b62
Abdelwahab Ayman Samy AbuGhaleb <abdelwahab.aym.aboels3od@gmail.com>
2025-11-17 11:57:34 +0200

    Update 6-4.java

--- Part 1/6. Conditional statements and conditional operation/Programming Exercise/6-4.java
+++ Part 1/6. Conditional statements and conditional operation/Programming Exercise/6-4.java
@@ -1,1 +1,16 @@
-
+//Write a program that prompts the user for a year. If the user inputs a number that is smaller than 2015, then the program prints the string "Ancient history!".
+import java.util.Scanner;
+ 
+public class SpeedingTicket {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        // Write your program here. 
+        System.out.println("Give speed:");
+        int speed=Integer.valueOf(scanner.nextLine());
+        if(speed>120){
+            System.out.println("Speeding ticket!");
+        }
+    }
+}
